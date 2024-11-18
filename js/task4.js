357055af14450802c06eb006d565bd1fc4532e80
Igoryakib <igoryakib@gmail.com>
2024-11-18 17:31:50 +0200

    fixed 4 task

--- js/task4.js
+++ js/task4.js
@@ -1,4 +1,4 @@
-const userValue = prompt('Enter your value to start the render');
+// const userValue = prompt('Enter your value to start the render');
 
 // Генерація динамічних елементів HTML та вставка їх у документ
-document.querySelector('body').insertAdjacentHTML('beforeend', `<h1>You generated dynamic site</h1> <p>Your value ${userValue}</p>`);
+// document.querySelector('body').insertAdjacentHTML('beforeend', `<h1>You generated dynamic site</h1> <p>Your value ${userValue}</p>`);
